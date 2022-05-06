0c6d60d6033e33e95859731143d148dfd4d325a0
HeisenBerg? <edwin_uestc@163.com>
2022-05-07 02:12:27 +0800

    Create 差异化定价.py

---
+++ 差异化定价.py
@@ -0,0 +1,13 @@
+https://s.taobao.com/search?q=%E6%B3%A5%E7%96%97%E8%86%8F&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_20220507&ie=utf8
+  
+泥疗膏 别人卖30 70 80买一送一  我卖135一盒
+
+
+1.那种受众需求很急的 立马到货
+
+2.没有明显的评价机制 
+
+3.增加幸福感的
+
+
+
